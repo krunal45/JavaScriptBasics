f69ee404040d05534f43e986c85954e001784b90
qaprayogshala <qaprayogshala@gmail.com>
2024-12-24 20:28:40 +0530

    dataTypeCode & dataTypeConversion Code

---
+++ dataTypeConversionExample.js
@@ -0,0 +1,21 @@
+let age = "18"
+console.log(typeof age);
+
+let ageInNumber = Number(age)
+console.log(typeof ageInNumber);
+
+age = "18ABC"
+console.log(typeof age);
+
+ageInNumber = Number(age)
+console.log(typeof ageInNumber);
+console.log(ageInNumber)
+
+flag = 1
+flagInBoolean = Boolean(flag)
+console.log(flagInBoolean);
+console.log(typeof flagInBoolean);
+
+flagInString = String(flag)
+console.log(flagInString);
+console.log(typeof flagInString);
---
+++ dataTypeExample.js
@@ -0,0 +1,12 @@
+{
+    var userName = 'Joy'
+    let id = 2
+    console.log('name > '+userName)
+}
+
+const studentId = 32
+console.log('studentId > '+studentId);
+userName = 'Krunal'
+console.log('name > '+userName);
+id = 3
+console.log('id > '+id);
